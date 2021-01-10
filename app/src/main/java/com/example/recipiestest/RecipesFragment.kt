8576187bad86fa5ff46ac858e3025e1a8a48bed2
Omar Alva Velarde <iamomar3a7x@gmail.com>
2021-01-10 16:56:59 -0600

    Added empty fragments, navigation and dependencies

---
+++ app/src/main/java/com/example/recipiestest/RecipesFragment.kt
@@ -0,0 +1,27 @@
+package com.example.recipiestest
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_recipes.*
+
+class RecipesFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_recipes, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navigate.setOnClickListener {
+            val action = RecipesFragmentDirections.actionRecipesFragmentToRecipeDetailFragment(1)
+            findNavController().navigate(action)
+        }
+    }
+}
